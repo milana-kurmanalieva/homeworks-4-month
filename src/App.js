8ba6fce243f6d591a4eaada830dc3f1c28af96b1
milana-kurmanalieva <you@example.com>
2024-02-09 22:27:30 +0600

    delete className

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import React from "react";
-
+import React from "react"
 function KatrinePhoto () {
   return(
-      <div className="block">
+      <div>
           <img src="https://i.imgur.com/MK3eW3As.jpg" alt="Katriene" width={150} height={150}/>
           <p>Katrine</p>
     </div>
@@ -11,7 +10,7 @@ function KatrinePhoto () {
 
 function MaiSakuragima() {
     return (
-        <div className="block">
+        <div>
             <img src="https://i.pinimg.com/originals/74/b3/67/74b367c4599f2bf627e1a10e8081c1dd.jpg" width={150}
                  height={150} alt=" sakuranjima Mai"/>
             <p>Sakuragima Mai</p>
@@ -21,7 +20,7 @@ function MaiSakuragima() {
 
 function KitagavaMarin (){
     return(
-        <div className="block">
+        <div>
             <img src="https://w.forfun.com/fetch/d7/d73b45a4ead91fcbf963a147aca92955.jpeg" width={150} height={150} alt="Kitagava Marin"/>
             <p>Kitagava Marin</p>
         </div>
@@ -30,7 +29,7 @@ function KitagavaMarin (){
 
 function Karuidzava () {
     return(
-        <div className="block">
+        <div>
             <img src="https://i.pinimg.com/736x/2a/bc/0a/2abc0a50bea5d28a59a24b6d76613c02.jpg" width={150} height={150} alt="Karuidzava"/>
             <p>Karuidzava</p>
         </div>
